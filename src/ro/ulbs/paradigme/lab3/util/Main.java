f0887b20be29903be778b591fc4216df8b3b6a83
Iasmina <iasminamaria.dobrota@ulbsibiu.ro>
2025-03-27 16:14:42 +0200

    lab3

--- src/ro/ulbs/paradigme/lab3/util/Main.java
+++ src/ro/ulbs/paradigme/lab3/util/Main.java
@@ -3,8 +3,8 @@ package ro.ulbs.paradigme.lab3.util;
 public class Main {
     public static void main(String[] args)
     {
-        PasswordMaker pm = new PasswordMaker("Iasmina");
-        System.out.println("Parola generata : " + pm.getPassword());
+        PasswordMaker pm = PasswordMaker.getInstance();
+        System.out.println("Parola generată: " + pm.getPassword());
 }
 
 }
--- src/ro/ulbs/paradigme/lab3/util/PasswordMaker.java
+++ src/ro/ulbs/paradigme/lab3/util/PasswordMaker.java
@@ -2,44 +2,26 @@ package ro.ulbs.paradigme.lab3.util;
 
 import java.util.Random;
 
-public class PasswordMaker
-{
-    private static final int MAGIC_NUMBER = new Random().nextInt(6) + 5;
+public class PasswordMaker {
+    private static final PasswordMaker instance;
+
     private final String magicString;
     private final String name;
-    private static PasswordMaker instance;
+    private static final int MAGIC_NUMBER = new Random().nextInt(6) + 5;
 
-    public PasswordMaker(String name)
-    {
+
+    static { // Bloc static pentru inițializare
+        instance = new PasswordMaker("DefaultUser");
+    }
+
+    private PasswordMaker(String name) { // Constructor privat
         this.name = name;
         this.magicString = StringRandomizer.generateRandomString(20);
     }
 
-    public static PasswordMaker getInstance(String name)
-    {
-        if(instance == null)
-        {
-            instance = new PasswordMaker(name);
-        }
+    public static PasswordMaker getInstance() {
         return instance;
     }
-    public String getPassword()
-    {
-        Random random = new Random();
 
-        String randomPart = StringRandomizer.generateRandomString(MAGIC_NUMBER);
-        StringBuilder magicPart = new StringBuilder();
-
-        for(int i = 0; i < 10; i++)
-        {
-            int index = random.nextInt(magicString.length());
-
-        }
-
-        String nameLength = String.valueOf(name.length());
-        int randomInt = random.nextInt(51);
-
-        return randomPart + magicPart + nameLength + randomInt;
-
-}
+    public String getPassword() { return name+MAGIC_NUMBER+magicString; }
 }
